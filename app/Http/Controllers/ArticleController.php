61ed5bc4969fa7b13893823f01d6b9d7c114681b
Victory Muz4k <muz4k.oo@gmail.com>
2020-07-12 21:48:39 +0300

    added create form

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use App\Article;
 
 class ArticleController extends Controller
@@ -17,5 +18,30 @@ class ArticleController extends Controller
     {
         $article = Article::findOrFail($id);
         return view('article.show', compact('article'));
+    }
+
+    public function create()
+    {
+        $article = new Article();
+
+        return view('article.create', compact('article'));
+    }
+
+    public function store(Request $request)
+    {
+        $data = $this->validate($request, [
+            'name' => 'required|unique:articles',
+            'body' => 'required|min:100',
+        ]);
+
+        $article = new Article();
+
+        $article->fill($data);
+
+        $article->save();
+
+
+        return redirect()
+            ->route('articles.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,11 @@ Route::get('/about','PageController@about');
 Route::get('/articles', 'ArticleController@index')
     ->name('articles.index');
 
+Route::post('/articles', 'ArticleController@store')
+    ->name('articles.store');
+
+Route::get('/articles/create', 'ArticleController@create')
+    ->name('articles.create');
+
 Route::get('/articles/{id}', 'ArticleController@show')
     ->name('article.show');
